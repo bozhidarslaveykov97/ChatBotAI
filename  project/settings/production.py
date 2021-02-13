ac7cf66d562df95b66828de3ac8364b6d4d1531e
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-02-13 03:08:13 +0200

    xx

---
+++  project/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+ALLOWED_HOSTS = [ 'www.vanesa-ai.com' ] # customize with your domain name
+
+DATABASES = {
+    'default': {
+
+    } # here the configuration for your database
+}
+
+STATIC_ROOT = '/home/vanesa-ai/ChatBotAI/static'
+MEDIA_ROOT = '/home/vanesa-ai/ChatBotAI/media'
