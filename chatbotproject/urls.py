a1a33ef65c33e7ca72c13c1cfbffda3ba5048210
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-02-22 22:32:57 +0200

    fx

--- chatbotproject/urls.py
+++ chatbotproject/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
      path('api/register', views.RegisterView.as_view(), name='auth_register'),
 
     path("api/send_chat", views.api_send_chat, name="api_send_chat"),
-    path("api/get_random_question", views.api_get_random_question, name="api_get_random_question")
+    path("api/get_random_question", views.api_get_random_question, name="api_get_random_question"),
 
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
 
